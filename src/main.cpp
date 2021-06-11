0c3dc43384fb8a713d8979e7385b387da25d667e
Jeremy M. Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2021-06-11 16:59:37 -0400

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,6 @@ std::mt19937 rng;
 std::uniform_real_distribution<fp> ux(-dx/2, dx/2);
 std::uniform_real_distribution<fp> uy(-dy/2, dy/2);
 
-// 0 for kmeans
-// 1 for threshold
-// 2 for fixed threshold
-#define MODE 2
-
 std::complex<fp> phi_n(int n, std::complex<fp> z, const std::complex<fp> c) {
 	while(n--) {
 		z = z * z + c;
@@ -70,28 +65,6 @@ bool is_period(const int n, std::complex<fp> z, const std::complex<fp> c) {
 
 // returns cycles in orbit or none if the point is outside the set
 std::optional<int> mandelbrot(fp x, fp y) {
-	/*std::complex<fp> c = std::complex<fp>(x, y);
-	{
-		std::complex<fp> z;
-		z = (1. + std::sqrt(1. - 4. * c)) / 2.;
-		if(std::abs(2. * z) < 1) {
-			return -1;
-		}
-		z = (1. - std::sqrt(1. - 4. * c)) / 2.;
-		if(std::abs(2. * z) < 1) {
-			return -1;
-		}
-	}
-	std::complex<fp> z = std::complex<fp>(0, 0);
-	int n = iterations;
-	while(n-- && std::norm(z) < 4) {
-		z = z * z + c;
-	}
-	if(std::norm(z) > 4) {
-		return {};
-	}
-	return 10;*/
-
 	/*
 	 * Return none for escapees
 	 * Return positive integer when period is known
@@ -101,7 +74,6 @@ std::optional<int> mandelbrot(fp x, fp y) {
 	std::complex<fp> z = std::complex<fp>(0, 0);
 	int n = iterations;
 	while(n-- && std::norm(z) < 4) {
-		//printf("%f+%fi\n", c.real(), c.imag());
 		z = z * z + c;
 	}
 	if(std::norm(z) > 4) {
@@ -114,59 +86,12 @@ std::optional<int> mandelbrot(fp x, fp y) {
 	 * Assume we've converged on an attractive fixed point here
 	 * Plug it into multiplier equation and check ...?
 	 */
-	//if(std::abs(2. * z) < 1) { // 1
-	//	return 1;
-	//}
-	//if(std::abs((2. * z) * (2. * (z * z + c))) < 1) { // 2
-	//	return 2;
-	//}
-	//for(int i = 1; i <= max_period; i++) {
-	//	if(std::abs(lambda(i, z, c)) < 1) {
-	//		return i;
-	//	}
-	//}
-	/*for(int i = max_period; i >= 1; i--) {
-		if(std::abs(lambda(i, z, c)) < 1) {
-			return i;
-		}
-	}*/
-	//if(std::abs(lambda(1, z, c)) < 1) {
-	//	return 1;
-	//}
-	//if(std::abs(lambda(2, z, c)) < 1 && std::abs(lambda(1, z, c)) >= 1) {
-	//	return 3;
-	//}
-	//if(std::abs(lambda(3, z, c)) < 1 && std::abs(lambda(2, z, c)) >= 1) {
-	//	return 4;
-	//}
-	//if(std::abs(lambda(1, z, c)) < 1 && std::abs(lambda(0, z, c)) >= 1) {
-	//	return 2;
-	//}
-	//if(is_period(1, z, c)) {
-	//	return 1;
-	//}
-	//if(is_period(2, z, c)) {
-	//	return 2;
-	//}
-	//if(is_period(3, z, c)) {
-	//	return 3;
-	//}
-	//for(int i = max_period; i >= 1; i--) {
 	for(int i = 1; i <= max_period; i++) {
 		if(is_period(i, z, c)) {
-			//if(true || std::norm(z) > 4) {
-			//	printf("\n>>%d %f %f+%fi\n", i, std::norm(z), c.real(), c.imag());
-			//	for(int j = 0; j < 5; j++) {
-			//		z = z * z + c;
-			//		printf("\n   - %f\n", std::norm(z));
-			//	}
-			//}
 			return i;
 		}
 	}
-	//assert(false);
 	return {};
-	return 0;
 }
 
 __attribute__((optimize("O1"))) // don't want ffast-math messing with this particular computation
@@ -191,11 +116,7 @@ pixel_t get_pixel(fp x, fp y) {
 			pixel = {v, v, v};
 		}
 		return pixel;
-		//bmp.set(i, j, pixel);
-		//bmp.set(i, h - j - 1, pixel);
 	} else {
-		//bmp.set(i, j, {255, 255, 255});
-		//bmp.set(i, h - j - 1, {255, 255, 255});
 		return {255, 255, 255};
 	}
 }
@@ -216,22 +137,9 @@ pixel_t sample(fp x, fp y) {
 }
 
 int main() {
-	///let p = mandelbrot(0.234375, 1);
-	///printf("%d %d", !!p, *p);
-	///return 0;
 	assert(byte_swap(0x11223344) == 0x44332211);
 	assert(byte_swap(pixel_t{0x11, 0x22, 0x33}) == (pixel_t{0x33, 0x22, 0x11}));
 	BMP bmp = BMP("test.bmp", w, h);
-	//pixel_t colors[] = {
-	//	{}
-	//};
-	//for(int j = 0; j <= h / 2; j++) {
-	//	fp y = ymin + ((fp)j / h) * (ymax - ymin);
-	//	let [_, y2] = get_coordinates(0, j);
-	//	printf("%0.20f %0.20f\n", y, y2);
-	//}
-	//return 1;
-	//for(int j = 0; j <= h / 2; j++) {
 	//[[maybe_unused]] pixel_t colors[] = {
 	//	{0, 0, 0},
 	//	{248, 86, 86},
@@ -246,7 +154,6 @@ int main() {
 			let color = sample(x, y);
 			bmp.set(i, j, color);
 		}
-		//bmp.write(); // debug stuff
 	}
 	bmp.write();
 }
