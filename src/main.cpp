db17d8aef6eba4c51de336bc8e6bb64717473064
Jeremy M. Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2021-06-13 13:04:14 -0400

    x

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ constexpr fp dx = (xmax - xmin) / w;
 constexpr fp dy = (ymax - ymin) / h;
 constexpr int iterations = 7000; //10000 * 2; //5000;
 constexpr int orbit_iterations = 5;
-constexpr bool AA = false;
+constexpr bool AA = true;
 constexpr int AA_samples = 10;
 constexpr int max_period = 20;
 
@@ -47,7 +47,7 @@ __attribute__((constructor)) void init_colors() {
 }
 
 enum class render_mode { brute_force, mariani };
-constexpr render_mode mode = render_mode::brute_force;
+constexpr render_mode mode = render_mode::mariani;
 
 constexpr int border_radius = 5;
 
@@ -141,7 +141,7 @@ std::tuple<fp, fp> get_coordinates(int i, int j) {
 	return {xmin + ((fp)i / w) * (xmax - xmin), ymin + ((fp)j / h) * (ymax - ymin)};
 }
 
-/*pixel_t get_pixel(fp x, fp y) {
+pixel_t get_pixel(fp x, fp y) {
 	if(let result = mandelbrot(x, y)) {
 		let period = result.value();
 		assert(period >= 0);
@@ -169,7 +169,7 @@ pixel_t sample(fp x, fp y) {
 	} else {
 		return get_pixel(x, y);
 	}
-}*/
+}
 
 pixel_t _get_color(int i, int j) {
 	assert(points[i][j].has_value());
@@ -278,59 +278,64 @@ void mariani_silver(int i, int j, int w, int h) {
 int main() {
 	// Render pipeline:
 	//   Mariani-silver figures out the mandelbrot main-body
-	//   
+	//   Edge detection ran to figure out where AA is needed
+	//   AA pass, parallel
+	//   Another pass to expose any missing detail as discovered by the AA pass
 	// todo: explore mariani-silver with escape time awareness
 	// todo: parallel mariani-silver
 	assert(byte_swap(0x11223344) == 0x44332211);
 	assert(byte_swap(pixel_t{0x11, 0x22, 0x33}) == (pixel_t{0x33, 0x22, 0x11}));
 	BMP bmp = BMP("test.bmp", w, h);
-	//for(int j = 0; j < h; j++) {
-	//	for(int i = 0; i < w; i++) {
-	//		if(i % 100 == 0) printf("\033[1K\r%0.2f%% %0.2f%%", (fp)(i + j * w) / (w * h) * 100, (fp)i / w * 100);
-	//		if(i % 100 == 0) fflush(stdout);
-	//		let [x, y] = get_coordinates(i, j);
-	//		let m = mandelbrot(x, y);
-	//		let color = m ? 0 : 255;
-	//		//let color = sample(x, y);
-	//		bmp.set(i, j, color);
-	//	}
-	//}
-	mariani_silver(0, 0, w, h);
-	puts("finished");
-	for(int i = 0; i < w; i++) {
+	if(mode == render_mode::brute_force) {
 		for(int j = 0; j < h; j++) {
-			assert(points[i][j].has_value());
+			for(int i = 0; i < w; i++) {
+				if(i % 100 == 0) printf("\033[1K\r%0.2f%% %0.2f%%", (fp)(i + j * w) / (w * h) * 100, (fp)i / w * 100);
+				if(i % 100 == 0) fflush(stdout);
+				let [x, y] = get_coordinates(i, j);
+				//let m = mandelbrot(x, y);
+				//let color = m ? 0 : 255;
+				let color = sample(x, y);
+				bmp.set(i, j, color);
+			}
 		}
-	}
-	for(int i = 0; i < w; i++) {
-		for(int j = 0; j < h; j++) {
-			bool center = points[i][j].value().escape_time.has_value();
-			bool has_white = false;
-			bool has_non_white = false;
-			for(int x = std::max(0, i - 1); x <= std::min(w - 1, i + 1); x++) {
-				for(int y = std::max(0, j - 1); y <= std::min(h - 1, j + 1); y++) {
-					if(x == i && y == j) continue;
-					if(points[x][y].value().escape_time.has_value()) {
-						has_white = true;
-					} else {
-						has_non_white = true;
+	} else {
+		mariani_silver(0, 0, w, h);
+		puts("finished");
+		for(int i = 0; i < w; i++) {
+			for(int j = 0; j < h; j++) {
+				assert(points[i][j].has_value());
+			}
+		}
+		for(int i = 0; i < w; i++) {
+			for(int j = 0; j < h; j++) {
+				bool center = points[i][j].value().escape_time.has_value();
+				bool has_white = false;
+				bool has_non_white = false;
+				for(int x = std::max(0, i - 1); x <= std::min(w - 1, i + 1); x++) {
+					for(int y = std::max(0, j - 1); y <= std::min(h - 1, j + 1); y++) {
+						if(x == i && y == j) continue;
+						if(points[x][y].value().escape_time.has_value()) {
+							has_white = true;
+						} else {
+							has_non_white = true;
+						}
+						if(has_white && has_non_white) goto b;
 					}
-					if(has_white && has_non_white) goto b;
 				}
-			}
-			b:
-			if((center && has_non_white) || (!center && has_white))
-			for(int x = std::max(0, i - border_radius); x <= std::min(w - 1, i + border_radius); x++) {
-				for(int y = std::max(0, j - border_radius); y <= std::min(h - 1, j + border_radius); y++) {
-					if((x-i)*(x-i) + (y-j)*(y-j) > border_radius*border_radius) continue;
-					wtf_mask[x][y] = true;
+				b:
+				if((center && has_non_white) || (!center && has_white))
+				for(int x = std::max(0, i - border_radius); x <= std::min(w - 1, i + border_radius); x++) {
+					for(int y = std::max(0, j - border_radius); y <= std::min(h - 1, j + border_radius); y++) {
+						if((x-i)*(x-i) + (y-j)*(y-j) > border_radius*border_radius) continue;
+						wtf_mask[x][y] = true;
+					}
 				}
 			}
 		}
-	}
-	for(int i = 0; i < w; i++) {
-		for(int j = 0; j < h; j++) {
-			bmp.set(i, j, get_color(i, j));
+		for(int i = 0; i < w; i++) {
+			for(int j = 0; j < h; j++) {
+				bmp.set(i, j, get_color(i, j));
+			}
 		}
 	}
 	bmp.write();
